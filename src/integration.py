68f0530eea4127b9ca3cf702dc091add426d07e6
Déhaies Nicolas <ndehaiesdit>
2015-05-02 21:48:59 +0200

    Done, quelques changements

--- src/integration.py
+++ src/integration.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as mp
 import numpy.random as npr
+import charg_f as cf
 
 #----------------------------------#
 #         INTEGRAL METHODS         #
@@ -12,7 +13,7 @@ def rect_meth(x, y, n, f):
     a = (y-x)/n
     s = 0.0
     t = np.arange(0, n)
-    for i in range(0,n):
+    for i in t:
         s += f(x + i*a)
     return a*s
     
@@ -30,21 +31,41 @@ def simpson_meth(x, y, n, f):
     s2 = 4*s2
     return (a/3)*(f(x) + s1 + s2 + f(y))
 
-def monte_carlo_meth(x, y, n, f):
-    max = 0.0
+def midpoint_meth(x,y,n,f):
     s = 0.0
-    for i in range(n+1):
-        a = f(x + y*float(i+1)/n)
-        if (max < a):
-            max = a
-    alpha = (b-a)*max
-    for i in range(n):
-        x_random = (b-a)*npr.rand() + x
-        y_random = max*npr.rand()
-        if (y_random <= max):
-            s += (alpha/n) * f(x_random)
-    return s
+    a = (y-x)/n
+    for k in np.arange(0,n):
+        s = s + f(x+(k*a)+(a/2.))
+    return a*s
 
+def comp_conv(f,a,b):
+    nmax = 100
+    
+    x = np.arange(1.0, nmax, 1.0)
+
+    t = np.arange(1.0, nmax, 1.0)
+    u = np.arange(1.0, nmax, 1.0)
+    v = np.arange(1.0, nmax, 1.0)
+
+    for i in np.arange(0.0,t.size):
+        t[i] = rect_meth(a,b,x[i],f)
+
+    for i in np.arange(0.0,u.size):
+        u[i] = simpson_meth(a,b,x[i],f)
+
+    for i in np.arange(0.0,v.size):
+        v[i] = midpoint_meth(a,b,x[i],f)
+
+    mp.clf()
+    mp.semilogx()
+    g = mp.plot(x, t, linewidth=1.0)
+    h = mp.plot(x, u, linewidth=1.0)
+    i = mp.plot(x, v, linewidth=1.0)
+  
+    mp.xlabel('Number of subdivision points')
+    mp.ylabel('Integral of f between a and b')
+    mp.legend((g,h,i),("Rectangle","Simpson","Middle Point"))
+    mp.show()
 
 #--------------------#
 #       LENGTH       #
@@ -54,10 +75,22 @@ def length(I, n, df, x, y):
     length = lambda x: np.sqrt(1 + (df(x)*df(x)))
     return I(x, y, n, length)
 
-def coef_to_fonction(A, B, C, D):
-    return lambda x: A*(x**3) + B*(x**2) + C*x + D
+# def coef_to_fonction(A, B, C, D):
+#     return lambda x: A*(x**3) + B*(x**2) + C*x + D
 
-def coef_to_deriv_fonction(A, B, C, D):
-    return lambda x: A*3*(x**2) + B*2*x + C    
+# def coef_to_deriv_fonction(A, B, C, D):
+#     return lambda x: A*3*(x**2) + B*2*x + C   
+    
 
-
+def derivate(Pol):
+    n = Pol.shape[0]
+    m = Pol.shape[1]
+    dPol = np.zeros([n,m])
+    
+    for i in np.arange(0,n):
+        dPol[i][0] = 0
+        dPol[i][1] = 3*Pol[i][0]
+        dPol[i][2] = 2*Pol[i][1]
+        dPol[i][3] = Pol[i][2]
+        
+    return dPol
